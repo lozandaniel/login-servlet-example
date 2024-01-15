170abb01ac19c2e94496d6c5ad158ef29526d383
Daniel Lozano <dl066522@gmail.com>
2024-01-15 17:24:08 -0500

    Diseño de la clase Usuario.

---
+++ src/java/Logica/Usuario.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Logica;
+
+/**
+ *
+ * @author dania
+ */
+public class Usuario {
+
+    private String nombre;
+    private String correo;
+    private String contraseña;
+
+    public Usuario(String nombre, String correo, String contraseña) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.contraseña = contraseña;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+}
